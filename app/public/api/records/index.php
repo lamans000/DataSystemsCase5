e5da04f3386fc4a8c9513a3be878eb2aae123f9b
lamans000 <ansonlam640@gmail.com>
2019-10-17 14:46:51 -0400

    1234

--- app/public/api/records/index.php
+++ app/public/api/records/index.php
@@ -10,13 +10,9 @@ if (isset($_GET['guid'])) {
   );
   $stmt->execute([$_GET['memberID']]);
 } else {
-<<<<<<< HEAD
-  $stmt = $db->prepare('SELECT * FROM Member');
-=======
   $stmt = $db->prepare('SELECT m.memberID, m.firstName,m.lastName,m.dob,m.gender,m.startDate,m.street,m.city,m.state,m.zip,m.email,m.workPhoneNumber,m.mobilePhoneNumber,m.jobTitle,m.radioNumber,m.stationNumber,m.isActive,c.certificationName
   FROM Member as m, Certifications as c, MemberCertifications as mc
   WHERE m.memberID = mc.memberID AND c.certificationID = mc.certificationID;');
->>>>>>> Sneha_10_8
   $stmt->execute();
 }
 $members = $stmt->fetchAll();
