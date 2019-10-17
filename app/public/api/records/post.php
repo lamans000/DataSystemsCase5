3a200bc0e05b94b820bd613d79d6c2f9f517729a
Colin121196 <coliooi@iu.edu>
2019-10-17 15:34:55 -0400

    Colin is a master

--- app/public/api/records/post.php
+++ app/public/api/records/post.php
@@ -6,12 +6,28 @@ $guid = Uuid::uuid4()->toString(); // i.e. 25769c6c-d34d-4bfe-ba98-e0ee856f3e7a
 $db = DbConnection::getConnection();
 // Step 2: Create & run the query
 $stmt = $db->prepare(
-  'INSERT INTO Member (memberID, firstName)
-  VALUES (?, ?)'
+  'INSERT INTO Member (memberID, firstName, lastName, dob, gender, startDate, street, city, state, zip, email, workPhoneNumber, mobilePhoneNumber, jobTitle, radioNumber, stationNumber, isActive)
+  VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)'
 );
 $stmt->execute([
   $guid,
-  $_POST['firstName']
+  $_POST['firstName'],
+  $_POST['lastName'],
+  $_POST['dob'],
+  $_POST['gender'],
+  $_POST['startDate'],
+  $_POST['street'],
+  $_POST['city'],
+  $_POST['state'],
+  $_POST['zip'],
+  $_POST['email'],
+  $_POST['workPhoneNumber'],
+  $_POST['mobilePhoneNumber'],
+  $_POST['jobTitle'],
+  $_POST['radioNumber'],
+  $_POST['stationNumber'],
+  $_POST['isActive']
+
 ]);
 // Step 4: Output
 header('HTTP/1.1 303 See Other');
