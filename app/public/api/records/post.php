f0f07087ae949dea2b1dee989a06cd974e9e1ca7
lamans000 <ansonlam640@gmail.com>
2019-10-22 14:03:27 -0400

    added update query

--- app/public/api/records/post.php
+++ app/public/api/records/post.php
@@ -6,7 +6,8 @@ $guid = Uuid::uuid4()->toString(); // i.e. 25769c6c-d34d-4bfe-ba98-e0ee856f3e7a
 $db = DbConnection::getConnection();
 // Step 2: Create & run the query
 $stmt = $db->prepare(
-  'INSERT INTO Member (memberID, firstName, lastName, dob, gender, startDate, street, city, state, zip, email, workPhoneNumber, mobilePhoneNumber, jobTitle, radioNumber, stationNumber, isActive)
+  'INSERT INTO Member (
+    memberID, firstName, lastName, dob, gender, startDate, street, city, state, zip, email, workPhoneNumber, mobilePhoneNumber, jobTitle, radioNumber, stationNumber, isActive)
   VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)'
 );
 $stmt->execute([
@@ -32,3 +33,16 @@ $stmt->execute([
 // Step 4: Output
 header('HTTP/1.1 303 See Other');
 header('Location: ../records/?guid=' . $guid);
+
+///Update query i don't know if this works
+// UPDATE Member
+// SET Member.firstName = ?,Member.lastName = ?,
+// Member.dob = ?, Member.gender = ?,
+// Member.startDate = ?, Member.street = ?,
+// Member.city = ?, Member.state = ?,
+// Member.zip = ?, Member.email = ?,
+// Member.workPhoneNumber = ?, Member.mobilePhoneNumber = ?,
+// Member.jobTitle = ?, Member.radioNumber = ?,
+// Member.stationNumber = ?, Member.isActive = ?
+// WHERE m.memberID = ?;
+////
