d1ab52b431f5102611aaa3c056801886db22b4c6
Colin121196 <coliooi@iu.edu>
2019-10-24 15:31:30 -0400

    Updated SQL statement in Update.php page

--- app/public/api/records/update.php
+++ app/public/api/records/update.php
@@ -5,15 +5,25 @@
 // Step 1: Get a datase connection from our help class
 $db = DbConnection::getConnection();
 // Step 2: Create & run the query
-$stmt = $db->prepare('UPDATE Member SET firstName = ? WHERE memberID = ?');
+$stmt = $db->prepare('UPDATE Member SET firstName = ?, lastName = ?, gender = ?, jobTitle = ?, startDate = ?, dob = ?, street = ?, city = ?, state = ?, zip = ?, email = ?, workPhoneNumber = ?, mobilePhoneNumber = ?, radioNumber = ?, stationNumber = ? WHERE memberID = ?');
 
 $stmt->execute([
-  // $guid,
   $_POST['firstName'],
-  // $_POST['lastName'],
+  $_POST['lastName'],
+  $_POST['gender'],
+  $_POST['jobTitle'],
+  $_POST['startDate'],
+  $_POST['dob'],
+  $_POST['street'],
+  $_POST['city'],
+  $_POST['state'],
+  $_POST['zip'],
+  $_POST['email'],
+  $_POST['workPhoneNumber'],
+  $_POST['mobilePhoneNumber'],
+  $_POST['radioNumber'],
+  $_POST['stationNumber'],
   $_POST['memberID']
-  // $_POST['dob']
-  // $_POST['gender'],
   // $_POST['startDate'],
   // $_POST['street'],
   // $_POST['city'],
